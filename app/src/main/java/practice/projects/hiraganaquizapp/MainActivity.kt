7095c9391ffa9d3c77b71bfabe2f497d139ec63e
Jerico <jericogantuangco200@gmail.com>
2022-02-28 11:44:04 +0800

    added basic flow to questions page

--- app/src/main/java/practice/projects/hiraganaquizapp/MainActivity.kt
+++ app/src/main/java/practice/projects/hiraganaquizapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package practice.projects.hiraganaquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val beginQuiz : Button = findViewById(R.id.btn_begin_quiz)
+        val name: EditText = findViewById(R.id.et_name)
+
+        beginQuiz.setOnClickListener {
+            if(name.text.isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            }else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
