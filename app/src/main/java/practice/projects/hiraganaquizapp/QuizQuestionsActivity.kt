85bcadd154da4851329b5169b446e52c10217fdf
Jerico <jericogantuangco200@gmail.com>
2022-03-03 11:49:27 +0800

    modified imports to all widgets

--- app/src/main/java/practice/projects/hiraganaquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/practice/projects/hiraganaquizapp/QuizQuestionsActivity.kt
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.ContextCompat
 
 class QuizQuestionsActivity : AppCompatActivity() {
